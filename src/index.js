f65012f352e53ed912c35d76f75f00ba46b8fce6
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-09 23:36:05 +0400

    Passing and Receiving props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail
+        author="Alex"
+        avatar={faker.image.avatar()}
+        timeAgo="Today at 5:00"
+        commentText="Nice blog post!"
+      />
+      <CommentDetail
+        author="Jane"
+        avatar={faker.image.avatar()}
+        timeAgo="Today at 3:33"
+        commentText="Good one."
+      />
+      <CommentDetail
+        author="Sam"
+        avatar={faker.image.avatar()}
+        timeAgo="Yesterday at 2:21"
+        commentText="Gotcha!"
+      />
     </div>
   );
 };
