e9d8180f6f85987a3def0b28279be3e79482d84e
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-11 05:41:45 +0400

    Conditionally rendering content

--- 2-season-checker/src/index.js
+++ 2-season-checker/src/index.js
@@ -5,16 +5,28 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
 
-    return <div>Geo:</div>;
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Lattitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
