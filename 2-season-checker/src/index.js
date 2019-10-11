b24ccf726ccf56a5780e9c118ec95308896ce804
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-12 03:58:07 +0400

    Index Js is updated

--- 2-season-checker/src/index.js
+++ 2-season-checker/src/index.js
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    this.state = { lat: null, errorMessage: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      error => this.setState({ errorMessage: error.message })
     );
   }
 
@@ -23,10 +19,14 @@ class App extends React.Component {
     }
 
     if (this.state.lat && !this.state.errorMessage) {
-      return <div>Lattitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <Spinner />>
+      </div>
+    );
   }
 }
 
