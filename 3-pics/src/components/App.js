80c4f6c8e04140dc057f6ba3d7a41837407457aa
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-14 14:37:26 +0400

    Async image loading from unsplash api is added

--- 3-pics/src/components/App.js
+++ 3-pics/src/components/App.js
@@ -1,15 +1,30 @@
 import React from "react";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    try {
+      const response = await unsplash.get("/search/photos", {
+        params: {
+          query: term
+        }
+      });
+
+      this.setState({ images: response.data.results });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
