ca906c598bc380d986d06ea0b721a887c2fad9dd
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-17 04:42:25 +0400

    VideoItem's completed

--- 4-videos/src/components/VideoItem.js
+++ 4-videos/src/components/VideoItem.js
@@ -10,7 +10,11 @@ const VideoItem = ({ videos, onVideoSelect }) => {
         key={video.id.videoId}
         onClick={() => onVideoSelect(video)}
       >
-        <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+        <img
+          className="ui image"
+          alt={video.snippet.title}
+          src={video.snippet.thumbnails.medium.url}
+        />
         <div className="content">
           <div className="header">{video.snippet.title}</div>
         </div>
