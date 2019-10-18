c4fe04d11c7dd0a3f0afe4d1f6ee766e3f3c8085
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-18 06:53:01 +0400

    Action Creators example is added

--- 5-redux/ReduxCycle.js
+++ 5-redux/ReduxCycle.js
@@ -1,1 +1,32 @@
 //! ACTION CREATOR (PERSON DROPPING OF THE FORM) ===> ACTION (THE FORM)  ===> DISPATCH (FORM RECIEVER) ===> REDUCERS (DEPARTMENTS) ===> STATE (COMPILED DEPARTMENT DATA)
+
+//* ACTION CREATORS:
+
+const createPolicy = (name, amount) => {
+  return {
+    type: "CREATE_POLICY",
+    payload: {
+      name,
+      amount
+    }
+  };
+};
+
+const deletePolicy = name => {
+  return {
+    type: "DELETE_POLICY",
+    payload: {
+      name
+    }
+  };
+};
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+  return {
+    type: "CREATE_CLAIM",
+    payload: {
+      name,
+      amountOfMoneyToCollect
+    }
+  };
+};
