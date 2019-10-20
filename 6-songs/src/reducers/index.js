a32633ee1fe19f7d8a5532473fd99819f4ab4632
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-20 05:01:01 +0400

    Reducer bug fix (typo)

--- 6-songs/src/reducers/index.js
+++ 6-songs/src/reducers/index.js
@@ -11,7 +11,7 @@ const songsReducer = () => {
 
 const selectedSongReducer = (selectedSong = null, action) => {
   if (action.type === "SONG_SELECTED") {
-    return selectedSong;
+    return action.payload.song;
   }
   return selectedSong;
 };
