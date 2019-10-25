2fc68c1dafbeac5642bb81b8c828bc21ada1b253
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-25 17:46:11 +0400

    GET_USER action is added

--- 7-posts/src/actions/index.js
+++ 7-posts/src/actions/index.js
@@ -3,5 +3,11 @@ import JSONPlaceholder from "../api/JSONPlaceholder";
 export const getPosts = () => async dispatch => {
   const response = await JSONPlaceholder.get("/posts");
 
-  dispatch({ type: "GET_POSTS", payload: response });
+  dispatch({ type: "GET_POSTS", payload: response.data });
+};
+
+export const getUser = id => async dispatch => {
+  const response = await JSONPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "GET_USER", payload: response.data });
 };
