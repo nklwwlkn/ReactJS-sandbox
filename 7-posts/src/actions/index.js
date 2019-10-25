d08c8daf927516063148b58b594c09ba9edde1cd
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-26 03:29:34 +0400

    git add 7-posts/src/actions/index.js ! Memoization replaced by custom action getPostsAndUsers

--- 7-posts/src/actions/index.js
+++ 7-posts/src/actions/index.js
@@ -1,16 +1,28 @@
 import _ from "lodash";
 import JSONPlaceholder from "../api/JSONPlaceholder";
 
+//! Лучший кейс, чем использование MEMOIZATION, позволяет обновлять полученные данные:
+
+export const getPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(getPosts());
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(userId => dispatch(getUser(userId)))
+    .value();
+};
+
+//*
+
 export const getPosts = () => async dispatch => {
   const response = await JSONPlaceholder.get("/posts");
 
   dispatch({ type: "GET_POSTS", payload: response.data });
 };
 
-export const getUser = id => async dispatch => _getUser(id, dispatch);
-
-const _getUser = _.memoize(async (id, dispatch) => {
+export const getUser = id => async dispatch => {
   const response = await JSONPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "GET_USER", payload: response.data });
-});
+};
