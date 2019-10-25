93687d5fac56e34e26350ec826d0957942291e25
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-26 02:12:54 +0400

    MEMOIZATION WITH LODASH

--- 7-posts/src/actions/index.js
+++ 7-posts/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import JSONPlaceholder from "../api/JSONPlaceholder";
 
 export const getPosts = () => async dispatch => {
@@ -6,8 +7,10 @@ export const getPosts = () => async dispatch => {
   dispatch({ type: "GET_POSTS", payload: response.data });
 };
 
-export const getUser = id => async dispatch => {
+export const getUser = id => async dispatch => _getUser(id, dispatch);
+
+const _getUser = _.memoize(async (id, dispatch) => {
   const response = await JSONPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "GET_USER", payload: response.data });
-};
+});
