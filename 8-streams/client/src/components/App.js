e717596a9fab566b7bb8da6491a22e7ac4f5e750
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-10-30 06:09:55 +0400

    Routes are connected to the components

--- 8-streams/client/src/components/App.js
+++ 8-streams/client/src/components/App.js
@@ -1,36 +1,25 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
-const partOne = () => {
-  return (
-    <div>
-      <h1>Hello from Home</h1>
-
-      <Link to="/next">
-        <button>Go to next</button>
-      </Link>
-    </div>
-  );
-};
-
-const partTwo = () => {
-  return (
-    <div>
-      <h1>Hello from another</h1>
-
-      <Link to="/">
-        <button>Go to Home</button>
-      </Link>
-    </div>
-  );
-};
+import Header from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
-        <Route path="/" exact component={partOne} />
-        <Route path="/next" component={partTwo} />
+        <div>
+          <Header />
+          <Route exact path="/" component={StreamList} />
+          <Route exact path="/streams/new" component={StreamCreate} />
+          <Route exact path="/streams/edit" component={StreamEdit} />
+          <Route exact path="/streams/delete" component={StreamDelete} />
+          <Route exact path="/streams/show" component={StreamShow} />
+        </div>
       </BrowserRouter>
     </div>
   );
