3fd2100be7e38da81e42b646f1c57b02805d0fcc
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-11-08 05:45:59 +0400

    Handling form submission with Redux-form

--- 8-streams/client/src/components/streams/StreamCreate.js
+++ 8-streams/client/src/components/streams/StreamCreate.js
@@ -2,15 +2,32 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+  onSubmit = formBody => {
+    console.log(formBody);
+  };
+
   renderInput = formProps => {
-    return <input {...formProps.input} />;
+    return (
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input {...formProps.input} />
+      </div>
+    );
   };
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter description"
+        />
+        <button className="primary ui button">Submit</button>
       </form>
     );
   }
