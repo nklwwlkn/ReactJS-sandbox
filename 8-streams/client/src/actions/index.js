6e6fb8e6f598cfbd95110862b19f6ba6c5d709e1
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-11-11 05:41:27 +0400

    Implementing other action creators

--- 8-streams/client/src/actions/index.js
+++ 8-streams/client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS
+} from "./types";
 
 export const signIn = userId => {
   return {
@@ -20,4 +28,28 @@ export const createStream = formValues => async dispatch => {
   const response = await streams.post("/streams", formValues);
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.patch(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
