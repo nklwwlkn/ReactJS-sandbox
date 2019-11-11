9a023e27c8346f45c33d0015bf8f8471f70315a8
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-11-11 04:51:26 +0400

    Redux Thunk is mounted

--- 8-streams/client/src/index.js
+++ 8-streams/client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
@@ -8,7 +9,7 @@ import reducers from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
