529b1d258dbc0f5a1292a80f0f7be9ab16803891
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-11-18 02:33:32 +0400

    Rendering list of streams

--- 8-streams/client/src/components/streams/StreamList.js
+++ 8-streams/client/src/components/streams/StreamList.js
@@ -8,18 +8,37 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera"></i>
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
+    console.log(this.props.streams);
     return (
       <div>
-        <h1>Stream List</h1>
+        <h2>Streams1</h2>
+        <div className="ui celled list">{this.renderList()}</div>
       </div>
     );
   }
 }
 
-export default connect(
-  null,
-  {
-    fetchStreams
-  }
-)(StreamList);
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams)
+  };
+};
+
+export default connect(mapStateToProps, {
+  fetchStreams
+})(StreamList);
