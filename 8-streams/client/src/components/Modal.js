0ed7000751a2030ddf301c4ad7d4edf03e9d877c
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-11-22 23:27:49 +0400

    Basic React Portal is implemented

---
+++ 8-streams/client/src/components/Modal.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Modal = props => {
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active">
+      <div className="ui standard visible modal active">asdasdasdasdasdsad</div>
+    </div>,
+    document.getElementById("modal")
+  );
+};
+
+export default Modal;
