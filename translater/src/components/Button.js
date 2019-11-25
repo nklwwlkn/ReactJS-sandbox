2fc7adffdb9376a9386b69a48e0d4743a4eb261f
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2019-11-25 23:06:07 +0400

    Context object is consumed by Consumer

--- translater/src/components/Button.js
+++ translater/src/components/Button.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    console.log(this.context);
-    return <button className="ui primary button">Submit</button>;
+    return (
+      <ColorContext.Consumer>
+        {color => (
+          <button className={`ui button ${color} `}>
+            <LanguageContext.Consumer>
+              {value => (value === "english" ? "Submit" : "Voorlaggen")}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
+    );
   }
 }
 
